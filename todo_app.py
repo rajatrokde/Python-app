829384754a0da58391aad690afa1043b677bf762
Rajat R Rokde <116082212+rajatrokde@users.noreply.github.com>
2025-05-19 16:13:04 +0530

    Update todo_app.py

--- todo_app.py
+++ todo_app.py
@@ -27,4 +27,5 @@ def delete(task_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=7000)
+   app.run(host="0.0.0.0", port=7000)
+
